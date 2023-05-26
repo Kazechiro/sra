6d53ae424fd85f393d8b0b300c124ccc348917ec
Kaio Barbosa <kaiob419@gmail.com>
2023-05-26 13:09:00 -0300

    mudança em novaTarefa

--- novaTarefa.php
+++ novaTarefa.php
@@ -5,12 +5,6 @@ $tarefa_nome = isset($_POST['nome'])? $_POST['nome'] : '';
 $desc_tarefa = isset($_POST['desc'])? $_POST['desc'] : '';
 
 
- 
-$verificar_tarefa ="SELECT nome_tarefa FROM tarefa WHERE nome_tarefa  = '$tarefa_nome"; 
-$query_verificar = mysqli_query($conexao, $verificar_tarefa);
-$dado = mysqli_fetch_row($query_verificar);
-
-
 
 $incluir = "INSERT INTO tarefa(nome_tarefa, desc_tarefa, concluida) 
 VALUES ('$tarefa_nome', '$desc_tarefa', 0)";
