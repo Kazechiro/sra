90bb9ed431b1181b6e75946ea74267adb93f5293
Luis Guilherme <133886753+Swainzada@users.noreply.github.com>
2023-06-11 16:16:59 -0300

    mudanças mudadas

--- criarRelatorio.php
+++ criarRelatorio.php
@@ -9,16 +9,78 @@ require('protect.php');
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="./css/styles.css">
     <title>Sistema de Relatórios</title>
 </head>
 <body>
+<header>
+        <nav class="nav-bar">
+            <div class="logo">
+                <h1>
+                    <ion-icon name="cafe-outline">
+
+                    </ion-icon>
+                    S.R.A
+                </h1>
+            </div>
+            <div class="nav-list">
+                <ul>
+                    <li class="nav-item">
+                        <a href="menu.html" class="nav-link">
+                            Início
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="principal.php" class="nav-link">
+                            Menu
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                            Sobre
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="mobile-menu-icon">
+                <button onclick="menuShow()">
+                    <img class="icon" src="assets/img/menu_white_36dp.svg" alt="">
+                </button>
+            </div>
+        </nav>
+        <div class="mobile-menu">
+            <ul>
+                <li class="nav-item">
+                    <a href="menu.html" class="nav-link">
+                        Início
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="principal.php" class="nav-link">
+                        Grupos
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        Sobre
+                    </a>
+                </li>
+            </ul>
+            
+        </div>
+    </header>
+    <center>
+        <br><br>
+        
+    <br><br><br><br>
     <h1>Relatório</h1>
     <form action="novoRelatorio.php" method="POST">
         <label for="titulo">Título:</label>
         <input type="text" name="titulo" id="titulo" required><br><br>
 
         <label for="descricao">Descrição:</label><br>
-        <textarea name="descricao" id="descricao" required></textarea><br><br>
+        <textarea name="descricao" id="descricao" boder=none rows=10 cols=50 maxlength="250" required></textarea><br><br>
 
         <input type="submit" value="Salvar">
     </form>
@@ -51,5 +113,9 @@ require('protect.php');
         // Fechar conexão
         $conexao->close();
     ?>
+<script src="./js/script.js"></script>
 </body>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
 </html>
