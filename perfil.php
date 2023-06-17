61ce2bf3ee755dde518b87e0de381a47c3249c7b
Kaio Barbosa <kaiob419@gmail.com>
2023-06-17 15:54:03 -0300

    perfil

--- perfil.php
+++ perfil.php
@@ -1,5 +1,7 @@
 <?php
-
+session_start();
+include('conexao.php');
+ // vai vir o protect.php
 
 
 ?>
@@ -11,6 +13,21 @@
   <title>Document</title>
 </head>
 <body>
+  <center> <h1>Perfil de <?php echo $_SESSION['nome'] ?></h1>
+  <?php
+  $sql_usuario = "SELECT * FROM USUARIO WHERE id_usuario = $_SESSION[id_usuario]";
+  $sql_query = $conn->prepare($sql_usuario);
+  $sql_query ->execute();
+
+  while ($row_usuario = $sql_query->fetch(PDO::FETCH_ASSOC)) {
+    echo "<li style='list-style: none'>";
+    echo "Nome: " . $row_usuario['nome'] . "<br>";
+    echo "Email: " . $row_usuario['email'] . "<br>";
+    echo "</li>";
+  }
   
+  
+  ?>
+  </center>
 </body>
 </html>
